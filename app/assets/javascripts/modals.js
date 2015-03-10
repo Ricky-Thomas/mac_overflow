29d3f207f3283f10b10be1a2852141b93be800ff
Pearl Shin & Ricky Thomas <pair+pearlshin.ricky-thomas@devbootcamp.com>
2015-03-10 18:12:11 -0400

    add login javascript for leanmodal

--- app/assets/javascripts/modals.js
+++ app/assets/javascripts/modals.js
@@ -1,3 +1,4 @@
 $(document).ready(function() {
-  $("#go_signup").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" })
+  $("#go_signup").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+  $("#go_login").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
 });
