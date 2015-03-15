076a03b6b7613e20ae43fc984ed91cff74a12e1e
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-03-15 16:56:07 -0400

    Added recipe id integer row for instruction migration

--- db/migrate/20150315203128_create_instructions.rb
+++ db/migrate/20150315203128_create_instructions.rb
@@ -2,6 +2,7 @@ class CreateInstructions < ActiveRecord::Migration
   def change
     create_table :instructions do |t|
       t.string :description
+      t.integer :recipe_id
 
       t.timestamps null: false
     end
