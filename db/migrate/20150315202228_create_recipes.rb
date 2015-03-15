9773cb81ec3fd3ea6d090edc7e5d2c8948b641cf
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-03-15 16:52:13 -0400

    Added user id integer row for recipe migration

--- db/migrate/20150315202228_create_recipes.rb
+++ db/migrate/20150315202228_create_recipes.rb
@@ -1,7 +1,8 @@
 class CreateRecipes < ActiveRecord::Migration
   def change
     create_table :recipes do |t|
-      t.string :name
+      t.string  :name
+      t.integer :user_id
 
       t.timestamps null: false
     end
