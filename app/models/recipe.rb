4dacbe71b2bc1d9d7a2f8467bbb8c645e0315168
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-03-16 16:40:17 -0400

    Added parse instructions method to recipe model

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -2,4 +2,11 @@ class Recipe < ActiveRecord::Base
   belongs_to :user
   has_many   :ingredients
   has_many   :instructions
+
+  def parse_ingredients!(ingredients_str)
+   ingredients_str.split(',').map(&:strip).uniq.each do |ingredient_str|
+   ingredient = Ingredient.where(name: ingredient_str).first_or_create
+   self.ingredients << ingredient
+   end
+  end
 end
